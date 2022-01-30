78764c9db9795135f40470b20b8ea66ca60629fe
mmasharov <masharov.m@gmail.com>
2022-01-30 17:45:10 +0500

    edit1

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import scrolledtext
+from pymedia import video_check
 
 def getffmpeg():
     filename = filedialog.askopenfilename()
@@ -9,21 +10,31 @@ def getvideodir():
     dirname = filedialog.askdirectory()
     txt1.insert(0, dirname)
 
+def do_check():
+    dirname = txt1.get()
+    ffmpeg = txt2.get()
+    #txtfield.insert(INSERT, dirname)
+    #txtfield.insert(INSERT, ffmpeg)
+    video_check(ffmpeg, dirname)
+    txtfield.insert(INSERT, 'All Done!')
+
 window = Tk()
 window.title('Videofile parameters check')
-window.geometry('580x200')
+window.geometry('680x200')
 lbl = Label(window, text='Getting videofile info: codec, size, fps, color encoding.', padx=2)
 txt1 = Entry(window, width=50)
 btn1 = Button(window, text='Выбрать путь к папке с видеофайлами', command=getvideodir)
 txt2 = Entry(window, width=50)
 btn2 = Button(window, text='Выбрать путь к ffmpeg', command=getffmpeg)
-txtfield = scrolledtext.ScrolledText(window, width=80, height=50)
+txtfield = scrolledtext.ScrolledText(window, width=80, height=5)
+btn3 = Button(window, text='Выполнить проверку', command=do_check)
 
-lbl.grid(column=0, row=0)
+lbl.grid(column=0, row=0, columnspan=2)
 txt1.grid(column=0, row=1)
 btn1.grid(column=1, row=1)
 txt2.grid(column=0, row=2)
 btn2.grid(column=1, row=2)
-txtfield.grid(column=1, row=3)
+txtfield.grid(column=0, row=3, columnspan=2)
+btn3.grid(column=0, row=4, columnspan=2)
 
 window.mainloop()
--- pymedia.py
+++ pymedia.py
@@ -1,19 +1,23 @@
 import os
 from pymediainfo import MediaInfo
 
-ffmpegp = 'D:\\ffmpeg\\bin\\ffmpeg.exe'
-fnames = os.walk(r'D:\FFOutput')
-for f in fnames:
-    for x in f[2]:
-        print(x, end=',')
-        fpath = f'D:\\FFOutput\\{x}'
-        media_info = MediaInfo.parse(fpath)
-        for track in media_info.tracks:
-            if track.track_type == "Video":
-                print(track.format, track.internet_media_type, track.width, track.height, track.frame_rate, track.color_space, track.chroma_subsampling)
-                if track.chroma_subsampling != '4:2:0':
-                    inputv = '-i ' + fpath
-                    outv = inputv[3:-4] + '_conv.mp4'
-                    os.system(f'{ffmpegp} {inputv} -pix_fmt yuv420p {outv}')
+#ffmpegp = 'D:\\ffmpeg\\bin\\ffmpeg.exe'
+#vdir = r'D:\FFOutput'
 
-print('All done!')
+def video_check(ffmpeg_path, videodir):
+    fnames = os.walk(videodir)
+    for f in fnames:
+        for x in f[2]:
+            print(x, end=',')
+            fpath = f'{videodir}\\{x}'
+            media_info = MediaInfo.parse(fpath)
+            for track in media_info.tracks:
+                if track.track_type == "Video":
+                    print(track.format, track.internet_media_type, track.width, track.height, track.frame_rate, track.color_space, track.chroma_subsampling)
+                    if track.chroma_subsampling != '4:2:0':
+                        inputv = '-i ' + fpath
+                        outv = inputv[3:-4] + '_conv.mp4'
+                        os.system(f'{ffmpeg_path} {inputv} -pix_fmt yuv420p {outv}')
+
+#video_check(ffmpegp, vdir)
+#print('All done!')
